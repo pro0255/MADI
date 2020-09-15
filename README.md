f31caadc7d27346c0505b942032547f3b66c3ec8
prokop <prokop@atlasgroup.cz>
2020-09-15 22:26:08 +0200

    chore: created readme and some others

---
+++ README.md
@@ -0,0 +1,9 @@
+Hello it is a READ ME for leactuers dedicated to MAD.
+
+Starts 16.9. 2020.
+
+
+
+# cv1:
+
+
--- cv1/main.py
+++ cv1/main.py
@@ -1,1 +1,16 @@
-if
+json = {"x": "x", "y": "y"}
+
+
+for i in json:
+    print(i)
+
+
+def fuck(pes):
+    print("fuck")
+
+def vojta():
+    print("ahoj")
+
+vojta()
+
+fuck("pes")
