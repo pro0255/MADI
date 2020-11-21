0815ffa619e4ba9db767b02a5e7a344f3c96c139
unknown <prokop.vojtech@gmail.com>
2020-11-21 17:10:20 +0100

    chore: constants update

--- cv9_gmail/constants/CONSTANTS.py
+++ cv9_gmail/constants/CONSTANTS.py
@@ -1,6 +1,6 @@
-EMAIL_MAX_COUNT = 1000
-PATH_TO_FILE = f"..\\data_sets\\output-{EMAIL_MAX_COUNT}-peta.csv"
-MATRIX_OUTPUT = f"..\\data_sets\\matrix_output\\output-{EMAIL_MAX_COUNT}-peta.txt"
-INSPECTION_OUTPUT = f"..\\data_sets\\inspection_output\\output-{EMAIL_MAX_COUNT}-peta.txt"
-GEPHI_OUTPUT = f"..\\data_sets\\gephi_output\\output-{EMAIL_MAX_COUNT}-peta.gexf"
-COLUMNS_IN_ROW = ["date", "from", "to", "cc"]
+EMAIL_MAX_COUNT = 2000
+PATH_TO_FILE = f"..\\data_sets\\output-{EMAIL_MAX_COUNT}.csv"
+MATRIX_OUTPUT = f"..\\data_sets\\matrix_output\\output-{EMAIL_MAX_COUNT}.txt"
+INSPECTION_OUTPUT = f"..\\data_sets\\inspection_output\\output-{EMAIL_MAX_COUNT}.txt"
+GEPHI_OUTPUT = f"..\\data_sets\\gephi_output\\output-{EMAIL_MAX_COUNT}.gexf"
+COLUMNS_IN_ROW = ["date", "from", "to", "cc", "tmp1"]
