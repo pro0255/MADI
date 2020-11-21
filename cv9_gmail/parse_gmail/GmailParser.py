ccc56d0ffdf8677f05bd6d5f50123cde8b4f44ce
unknown <prokop.vojtech@gmail.com>
2020-11-21 14:08:58 +0100

    feat: parser wip

--- cv9_gmail/parse_gmail/GmailParser.py
+++ cv9_gmail/parse_gmail/GmailParser.py
@@ -1,19 +1,76 @@
 import pandas as pd
 from constants.CONSTANTS import COLUMNS_IN_ROW
-
+import math
+import numpy as np
+from itertools import product
 
 
 class GmailParser():
     def __init__(self):
-        pass
+        self.emails = []
+        self.matrix = None
 
     def parse(self, path_to_file, names=COLUMNS_IN_ROW):
         raw_gmail_data = pd.read_csv(path_to_file, delimiter=';', encoding='unicode_escape', header=None)
         raw_gmail_data.columns = names
-        self.clean_data(raw_gmail_data)
+        self.build_matrix(raw_gmail_data)
 
-    def clean_data(data, raw_data):
-        print(raw_data.head())
-        print(raw_data.iloc[0])
+    def parse_from_or_to_mail(self, from_or_to_string):
+        lIndex = from_or_to_string.find('<')
+        rIndex = from_or_to_string.find('>')
+
+        if lIndex == -1:
+            name = None
+            mail = from_or_to_string.strip()
+        else:
+            name = from_or_to_string[0:lIndex].strip()
+            mail = from_or_to_string[lIndex+1:rIndex]    
+        return (mail, name)
+
+    def parse_cc(self, cc_string):
+        result = None if pd.isnull(cc_string) else [cc.strip() for cc in cc_string.split(',')]
+        return result
+
+    def parse_row(self, row):
+        return (self.parse_from_or_to_mail(row['from']),self.parse_from_or_to_mail(row['to']), self.parse_cc(row['cc']))
+
+    def create_emails_set(self, raw_data):
+        for index, row in list(raw_data.iterrows())[0:2]:
+            self.process_row_with_add_to_emails(*self.parse_row(row))
+
+    def process_row_with_add_to_emails(self, from_mail, to_mail, cc_mails):
+        self.emails.append(from_mail[0])
+        self.emails.append(to_mail[0])
+        if cc_mails is not None:
+            self.emails += cc_mails
+        self.emails = list(set(self.emails)) #unique
+
+    def create_matrix_from_set(self, raw_data, emails_set):
+        lE = len(emails_set)
+        self.matrix = np.zeros(shape=(lE, lE))
+        dic_email_index = {email:index for index,email in enumerate(emails_set)}
+        for index, row in list(raw_data.iterrows())[0:2]:
+            self.process_row_with_add_to_matrix(*self.parse_row(row), dic_email_index)
+        
+    def process_row_with_add_to_matrix(self, from_mail, to_mail, cc_mails, emails_dic_email_index):
+        current_emails = [from_mail[0], to_mail[0]]
+        if cc_mails is not None:
+            current_emails += cc_mails
+        prod = list(product(current_emails,current_emails))
+        result = list(filter(lambda a: a[0] != a[1], prod))
+
+        for tup in result:
+            i = emails_dic_email_index[tup[0]]
+            j = emails_dic_email_index[tup[1]]
+            self.matrix[i][j] += 1
+
+
+
+    def build_matrix(self, raw_data):
+        self.create_emails_set(raw_data)
+        self.create_matrix_from_set(raw_data, self.emails)
+        output = pd.DataFrame(self.matrix, columns=self.emails, index=self.emails).to_string()
+        with open("matrix.txt", "w") as file1: 
+            file1.writelines(output) 
+        return (self.emails, self.matrix)
     
- 
